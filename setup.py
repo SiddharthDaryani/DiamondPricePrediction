921377edd8af319884c5210356ac277a6dcaa590
SiddharthDaryani <siddharthdaryani49@gmail.com>
2023-10-28 20:05:09 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name= "DiamondPricePrediction",
+    version= '0.0.1',
+    author= 'siddharth daryani',
+    author_email= 'siddharthdaryani49@gmail.com',
+    packages= find_packages()
+)
